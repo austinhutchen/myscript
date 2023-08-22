0a2ef116f7acb893ef85461108cbfc0a6c4e98b6
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-21 20:32:33 -0700

    default

--- LCDinit.ino
+++ LCDinit.ino
@@ -3,6 +3,14 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 SoftwareSerial mySerial(2, 3);
+struct user {
+  unsigned long buf;
+  user() { buf = 0; }
+  unsigned long func() { return buf; }
+  refresh() { buf = mySerial.read(); }
+};
+user *usr = new user();
+
 void setup() {
   mySerial.begin(9600); // Setting the baud rate of Software Serial Library
   Serial.begin(9600);   // Setting the baud rate of Serial Monitor
@@ -14,9 +22,9 @@ void setup() {
 
 void loop() {
   // print the number of seconds since reset:
+  usr->refresh();
   lcd.setCursor(0, 0);
-  auto buf = mySerial.read();
-  lcd.print(buf);
+  lcd.print(usr->func());
   lcd.setCursor(0, 1);
   lcd.print(millis() / 1000);
   delay(100);
