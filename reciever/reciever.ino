839ac59c62fb4ea805d4b00e8201e282c4ae34ea
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-22 09:04:54 -0700

    remote control support

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,6 +1,21 @@
+
+#include "IRremote.h"
+#include "Stepper.h"
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 #include "./BITS.c"
+/*----- Variables, Pins -----*/
+#define STEPS 32  // Number of steps per revolution of Internal shaft
+int Steps2Take;   // 2048 = 1 Revolution
+int receiver = 5; // Signal Pin of IR receiver to Arduino Digital Pin 6
+
+/*-----( Declare objects )-----*/
+// Setup of proper sequencing for Motor Driver Pins
+// In1, In2, In3, In4 in the sequence 1-3-2-4
+
+Stepper small_stepper(STEPS, 0, 1, 4, 6);
+IRrecv irrecv(receiver); // create instance of 'irrecv'
+decode_results results;  // create instance of 'decode_results'
 #define UCHAR_MAX pow(2, 8) - 1
 // initialize the library with the numbers of the interface pins11
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -43,6 +58,7 @@ struct user {
 user *usr = new user();
 
 void setup() {
+  irrecv.enableIRIn(); // Start the receiver
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   mySerial.begin(9600); // Setting the baud rate of Software Serial Library
@@ -54,6 +70,30 @@ void setup() {
 }
 
 void loop() {
+  if (irrecv.decode(&results)) // have we received an IR signal?
+  {
+    switch (results.value)
+    {
+    case 0xFFA857:                 // VOL+ button pressed
+      small_stepper.setSpeed(500); // Max seems to be 500
+      Steps2Take = 2048;           // Rotate CW
+      small_stepper.step(Steps2Take);
+      delay(2000);
+      break;
+    case 0xFF629D: // VOL- button pressed
+      small_stepper.setSpeed(500);
+      Steps2Take = -2048; // Rotate CCW
+      small_stepper.step(Steps2Take);
+      delay(2000);
+      break;
+    }
+
+    irrecv.resume(); // receive the next value
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+  }
   if (mySerial.isListening()) {
     // print the number of seconds since reset:
     usr->refresh();
@@ -66,7 +106,7 @@ void loop() {
     }
     lcd.setCursor(0, 1);
     lcd.print(millis() / 1000);
-    lcd.setCursor(8,1);
+    lcd.setCursor(8, 1);
     // bits in current read buffer
     lcd.print(leftBitCount(t));
     delay(100);
