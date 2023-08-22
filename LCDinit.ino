663ee6205ac0844231c0b67557a569d715cabccb
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-22 00:11:30 -0700

    test

--- LCDinit.ino
+++ LCDinit.ino
@@ -1,20 +1,39 @@
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
-
 #define INT_MAX pow(2, 32) - 1
 #define ulong unsigned long
 // initialize the library with the numbers of the interface pins11
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-SoftwareSerial mySerial(2, 3);
+const byte rxPin = 2;
+const byte txPin = 3;
+SoftwareSerial mySerial(rxPin, txPin);
+
 struct user {
-  unsigned long buf;
-  user() { buf = 0; }
-  unsigned long getbuf() { return buf; }
-  refresh() { buf = mySerial.read(); }
+  char buf;
+  char **str;
+  int size;
+  user() { buf = size = str = 0x0; }
+  char getbuf() {
+    str += buf;
+    this->size += 1;
+    return buf;
+  }
+  void refresh() { buf = mySerial.read(); }
+  void prnt() {
+    int i = 0;
+    while (i < 8 && i != this->size) {
+      lcd.print(this->str[i]);
+      lcd.setCursor(0, i);
+      i++;
+      delay(200);
+    }
+  }
 };
 user *usr = new user();
 
 void setup() {
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
   mySerial.begin(9600); // Setting the baud rate of Software Serial Library
   Serial.begin(9600);   // Setting the baud rate of Serial Monitor
   Serial.println("starting up...");
@@ -24,18 +43,26 @@ void setup() {
 }
 
 void loop() {
-  // print the number of seconds since reset:
-  usr->refresh();
-  lcd.setCursor(0, 0);
-  ulong t = usr->getbuf();
-  if (t != INT_MAX) {
-    lcd.print(t);
+  if (mySerial.isListening()) {
+    // print the number of seconds since reset:
+    usr->refresh();
+    lcd.setCursor(0, 0);
+    char t = usr->getbuf();
+    if (t != WCHAR_MAX) {
+      usr->prnt();
+    } else {
+      lcd.print("ERR");
+    }
+    lcd.setCursor(0, 1);
+    lcd.print(millis() / 1000);
+    delay(100);
+    lcd.clear();
+
   } else {
-    lcd.print("ERR");
+    delete usr;
+    usr = 0x0;
+    return;
   }
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
-  delay(100);
-  lcd.clear();
-  // usr need to be deallocated 
+
+  // usr need to be deallocated
 }
