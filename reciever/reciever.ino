634301f45a901339ecb9736925c8739558bec629
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-23 11:52:37 -0700

    communication is good

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -9,15 +9,10 @@
 int Steps2Take;   // 2048 = 1 Revolution
 int receiver = 5; // Signal Pin of IR receiver to Arduino Digital Pin 6
 #define ushort unsigned short
+#define UCHAR_MAX pow(2, 8) - 1
 /*-----( Declare objects )-----*/
 // Setup of proper sequencing for Motor Driver Pins
 // In1, In2, In3, In4 in the sequence 1-3-2-4
-
-Stepper small_stepper(STEPS, 2, 3, 4, 6);
-IRrecv irrecv(receiver); // create instance of 'irrecv'
-decode_results results;  // create instance of 'decode_results'
-#define UCHAR_MAX pow(2, 8) - 1
-// initialize the library with the numbers of the interface pins11
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 const byte rxPin = 0;
 const byte txPin = 1;
@@ -27,6 +22,10 @@ typedef char *iterator;
 iterator array_begin(char a[]) { return &a[0]; }
 iterator array_end(char a[], int n) { return &a[n]; }
 iterator array_next(iterator i) { return ++i; }
+Stepper small_stepper(STEPS, 2, 3, 4, 6);
+IRrecv irrecv(receiver); // create instance of 'irrecv'
+decode_results results;  // create instance of 'decode_results'
+// initialize the library with the numbers of the interface pins11
 
 struct user {
   char buf;
@@ -42,12 +41,11 @@ struct user {
     return buf;
   }
 
-  void wrap(){
-    if(this->index==15){
-    this->index=0;
+  void wrap() {
+    if (this->index == 15) {
+      this->index = 0;
     }
-      return;
-    
+    return;
   }
   void refresh() { buf = mySerial.read(); }
   void prnt() {
@@ -55,8 +53,8 @@ struct user {
     iterator it = array_begin(this->str);
     iterator end = array_end(this->str, 15);
     for (; it != end; it = array_next(it)) {
-      lcd.print(String(*it));
       lcd.setCursor(i, 0);
+      lcd.print(*it);
       i++;
     }
     lcd.setCursor(8, 1);
